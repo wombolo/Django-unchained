9d7595a21e9899c13f481ccd2326971dd702ecf7
Wombolo <elkenzom1@gmail.com>
2019-09-13 11:53:53 +0100

    feat(create_articles): implement admin can create articles
    - ensures admin can create articles
    
    [Delivers #168288172]

---
+++ blog/views.py
@@ -0,0 +1,20 @@
+from django.http import Http404
+from django.shortcuts import render
+
+# Create your views here.
+
+from .models import BlogPost
+
+def blog_post_detail_page(request, post_id):
+    try:
+        obj = BlogPost.objects.get(id=post_id)
+
+    except BlogPost.DoesNotExist:
+        raise Http404
+    except ValueError:
+        raise Http404
+
+    template_name = 'blog_post_detail.html'
+    context = {"object": obj, "title": obj.title}
+
+    return render(request, template_name, context)
